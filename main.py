1743ee798219c1c587e2b24220d64c74d710ccf4
Atrain25 <Ajtrain25@gmail.com>
2025-12-31 00:35:51 -0500

    Add gunicorn and deployment files

--- main.py
+++ main.py
@@ -783,5 +783,8 @@ def search():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', 'true').lower() == 'true'
+    app.run(debug=debug, host='0.0.0.0', port=port)
 
